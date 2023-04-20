304a17d83668dca1b887804064527bb79d0404b0
VladimirEnot <105981763+VladimirEnot@users.noreply.github.com>
2023-04-20 23:33:12 +0300

    with the corrected

--- src/main/java/ru/netology/stats/StatService.java
+++ src/main/java/ru/netology/stats/StatService.java
@@ -1,26 +1,67 @@
 package ru.netology.stats;
+
 public class StatService {
     public long sum(long[] sales) {
-        long sum = 0;
+        long result = 0;
         for (long sale : sales) {
-            sum += sale;
+            result += sale;
         }
-        return sum;
+        return result;
 
     }
-    public long avarage(long[] sales){
-        return  sum(sales)/12;
+
+    public long avarage(long[] sales) {
+        return sum(sales) / sales.length;
     }
 
-    public int avgLess(long[] sales){
-        long avg = avarage(sales);
-        int count =0;
-        for (long sale : sales) {
-            if (sale <avg){
-                count++;
+    public int monthMaximumSale(long[] sales) {
+        int monthMaximum = 0;
+        long saleMaximum = sales[0];
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= saleMaximum) {
+                monthMaximum = i;
+                saleMaximum = sales[i];
             }
         }
-        return count;
+        return monthMaximum + 1;
+
     }
 
+    public int monthMinimumSale(long[] sales) {
+        int monthMinimum = 0;
+        long saleMinimum = sales[0];
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= saleMinimum) {
+                monthMinimum = i;
+                saleMinimum = sales[i];
+            }
+        }
+        return monthMinimum + 1;
+    }
+
+    public int calcMonthSaleBellowAverage(long[] sales) {
+        long avarageSale = avarage(sales);
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < avarageSale) {
+                counter++;
+
+            }
+        }
+        return counter;
+    }
+
+    public int calcMonthSaleAboveAverage(long[] sales) {
+        long avarageSale = avarage(sales);
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > avarageSale) {
+                counter++;
+
+            }
+        }
+        return counter;
+
+
+    }
 }
--- src/test/java/ru/netology/statistic/StatServiceTest.java
+++ src/test/java/ru/netology/statistic/StatServiceTest.java
@@ -2,32 +2,79 @@ package ru.netology.statistic;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.stats.StatService;
 
 public class StatServiceTest {
 
     @Test
-    public void sum() {
+    public void testSum() {
         StatService service = new StatService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long actual = service.sum(sales);
         long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
 
 
-        assertEquals(actual, expected);
+        Assertions.assertEquals(actual, expected);
     }
 
     @Test
-    public void avarage() {
+    public void testAvarage() {
         StatService service = new StatService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long actual = service.avarage(sales);
         long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
 
 
-        assertEquals(actual, expected);
+        Assertions.assertEquals(actual, expected);
+    }
 
+    @Test
+    public void testMonthMaximumSale() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.monthMaximumSale(sales);
+        long expected = 8;
+
+
+        Assertions.assertEquals(actual, expected);
 
     }
+
+    @Test
+    public void testMonthMinimumSale() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.monthMinimumSale(sales);
+        long expected = 9;
+
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testcalcMonthSaleBellowAverage() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.calcMonthSaleBellowAverage(sales);
+        long expected = 5;
+
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testcalcMonthSaleAboveAverage() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.calcMonthSaleAboveAverage(sales);
+        long expected = 5;
+
+
+        Assertions.assertEquals(actual, expected);
+    }
 }
+
+
+
